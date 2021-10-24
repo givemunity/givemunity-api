65266b4cf3e9154aed98c4af38f6292205477a5d
ckho <me@ckho.info>
2021-10-24 14:14:47 +0800

    feat: store record to dynamodb

--- pocService/func/createDonorResponseV1/index.js
+++ pocService/func/createDonorResponseV1/index.js
@@ -9,19 +9,25 @@ const inputOutputLogger = require('./middlewares/inputOutputLogger')
 const AWSXRay = require('aws-xray-sdk-core')
 const AWS = AWSXRay.captureAWS(require('aws-sdk'))
 AWS.config.update({ region: 'ap-east-1' })
+const docClient = new AWS.DynamoDB.DocumentClient()
 
+const { v4: uuidv4 } = require('uuid')
+
+const tableName = process.env.TABLE_NAME
 
 const baseHandler = async (event, context) => {
-    console.log(event.rawBody);
-
-    if (error) {
-        throw createError(400, 'error detail');
+    let record = {
+        id: uuidv4(),
+        name: event.body.name,
+        email: event.body.email,
+        amount: event.body.amount
     }
 
+    await docClient.put({ TableName: tableName, Item: record }).promise()
 
-    const response = { result: 'success', message: 'successfully stored the data' };
-    return { statusCode: 201, body: response };
-};
+    const response = { result: 'success', message: 'successfully stored the data', link: null }
+    return { statusCode: 201, body: response }
+}
 
 const handler = middy(baseHandler)
     .use(jsonBodyParser())
